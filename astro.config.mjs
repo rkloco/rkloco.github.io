6d2de552733782bc3acb2b175fc7ef3dbbc49c04
rkloco <emailronitk@gmail.com>
2025-05-05 22:48:41 -0400

    custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  site: 'https://rkloco.github.io',
+  site: 'www.ronitkapur.org',
 });
